f292f3dfa5ef89c6fcafab2a65faccac9e9a3d2c
lph177891840 <177891840@qq.com>
2016-12-05 22:26:54 +0800

    test

--- client.js
+++
@@ -1,8 +0,0 @@
-var ws=new WebSocket("ws://localhost:8181");
-ws.onopen=function(e){
-	console.log('Connection to server opened');
-	alert('Connection to server opened');
-};
-function sendMessage(){
-	ws.send("123");
-}
