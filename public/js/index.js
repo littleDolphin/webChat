2af7ba3b31bda1b9354476987c43873b99e47f54
lph177891840 <177891840@qq.com>
2017-03-07 23:18:01 +0800

    1

--- public/js/index.js
+++ public/js/index.js
@@ -1,49 +1,92 @@
-var userName='user'+Math.round(Math.random()*10);
+var userName='user'+Math.round(Math.random()*10000);
 var socket = io();
-alert(userName);
-socket.emit('connect',userName);
+
+socket.emit('myconnect',userName);
 var username=document.getElementById('username');
-
-var button=document.getElementById('button');
-var input=document.getElementById('input');
-var message_ul=document.getElementById('message_ul');
+username.value=userName;
 
 
-button.onclick=function(){
-	if(input.value!=''){
-		var jsonMessage={
-			'username':username.value,
-			'message':input.value
-		}
-		socket.emit('chat',JSON.stringify(jsonMessage));
-	}
+// var message_ul=document.getElementById('message_ul');
+
+username.onkeypress=function(e){
+	alert(345);
 };
 
-input.onkeypress=function(e){
-	if(e.keyCode==13 && input.value!=''){
-		var jsonMessage={
-			'username':username.value,
-			'message':input.value
-		}
-		socket.emit('chat',JSON.stringify(jsonMessage));
-	}	
-};
+// button.onclick=function(){
+// 	if(input.value!== '' ){
+// 		var jsonMessage={
+// 			'username':username.value,
+// 			'message':input.value
+// 		};
+// 		socket.emit('chat',JSON.stringify(jsonMessage));
+// 	}
+// };
+
+// input.onkeypress=function(e){
+// 	if(e.keyCode==13 && input.value!==''){
+// 		var jsonMessage={
+// 			'username':username.value,
+// 			'message':input.value
+// 		};
+// 		socket.emit('chat',JSON.stringify(jsonMessage));
+// 	}
+// };
 
 socket.on('send',function(jsonMessage){
-	var jsonObject=JSON.parse(jsonMessage)
-	var username=jsonObject.username+': ';
+	var jsonObject=JSON.parse(jsonMessage);
+	var username=jsonObject.username;
 	var message=jsonObject.message;
-	appendMessage(username,message);
+	// appendMessage(username,message);
 	input.value='';
 });
 
+socket.on('notice_login',function(userName){
+	// appendMessage(userName,"加入会话");
+});
+socket.on('notice_logout',function(userName){
+	// appendMessage(userName,"离开会话");
+});
+
+
 function appendMessage(username,message){
 	var li = document.createElement("li");
-	var span_logo=document.createElement("span");
+	var user_logo=document.createElement("span");
+	user_logo.className='user_logo';
+	user_logo.innerHTML=username;
+
 	var span_message=document.createElement("span");
-	span_logo.innerHTML=username;
+	span_message.className='send';
 	span_message.innerHTML = message;
-	li.appendChild(span_logo);
+	var arrow=document.createElement("span");
+	span_message.appendChild(arrow);
+	arrow.className='leftArrow';
+	li.appendChild(user_logo);
 	li.appendChild(span_message);
 	message_ul.appendChild(li);
 }
+
+
+//左侧添加一个房间或对话
+function appendSelect(name){
+	var ul=document.getElementById("select_div").getElementsByTagName("ul")[0];
+	var li = document.createElement("li");
+	li.innerHTML=name;
+	li.selectName=name;
+	ul.appendChild(li);
+}
+//右侧添加一个对应房间或对话的消息列表和输入input
+function appendOneSelect(name){
+	var message_div=document.getElementById("message_div");
+	message_div.innerHTML='<div class="one_select" selectName="">'+
+								'<div class="message_list">'+
+									'<ul class="message_ul">'+
+									'</ul>'+
+								'</div>'+
+								'<div class="message_input">'+
+									'<input type="text" class="input">'+
+									'<button class="button">dsf</button>'+
+								'</div>'+
+							'</div>';
+}
+appendSelect('34');
+appendOneSelect('34');
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
+//引入express模块，进行session控制等
 var express = require('express');
 var app=new express();
 var http = require('http').Server(app);
-var io=require('socket.io')(http);
 
+// var session=require('express-session');
+
+// app.use(express.session());
 app.use(express.static(__dirname+'/public'));
 
 app.get('/', function(req, res){
@@ -10,8 +13,12 @@ app.get('/', function(req, res){
 });
 
 
+
+var io=require('socket.io')(http);
 /**
 	自定义的事件:
+		notice_login:系统消息
+
 		chat:在聊天室里的聊天
 		send:服务端发给客户端
 		get: 客户端发给服务端(服务端获取)
@@ -21,49 +28,111 @@ app.get('/', function(req, res){
 	username
 	message
 */
-var users=new Object();
 
-var count=0;
-var userName;
+
+/* 用一种数据结构(图)把username和userid关联起来*/
+
+
+
+var users={
+	//用户名userName和套接字id(socketId)存放在name_id中
+	userCount: 0,
+	name_id: {},
+	addUser: function(userName,userId){
+		if(users.name_id[userName] === undefined){
+			users.name_id[userName]=userId;
+			users.userCount++;
+		}
+	},
+	deleteUser: function(userName){
+		if(users.name_id[userName] !== undefined){
+			delete users.name_id[userName];
+			users.userCount--;
+		}
+	},
+	hasUser: function(userName){
+		if(users.name_id[userName] ===undefined){
+			return true;
+		}else{
+			return false;
+		}
+	},
+	getUserId: function(userName){
+		if(users.name_id[userName] !==undefined){
+			return users.name_id[userName];
+		}else{
+			return undefined;
+		}
+	}
+};
+// users.addUser('test','1434');
+// console.log('-----------:' + users.name_id['test']);
+// console.log('-----------:' + users.hasUser('test'));
+// users.deleteUser('test');
+// console.log('-----------:' + users.name_id['test']);
+// console.log('-----------:' + users.hasUser('test'));
+
+
 
 io.sockets.on('connection', function(socket){
 
 	//io.emit('事件名',msg)给全部连接发送消息
 	//socket.emit('事件名',msg)给该连接发送消息
 
-	// console.log('socketid: ' + socket.id);
-	// userName='user'+count;
+	//用户连接，浏览器端会触发一个userConnect事件
+	socket.on('userConnect', function(username){
+		users.addUser(username,socket.id);
+		console.log(username+' 登入,'+'在线人数'+users.userCount);
+	});
 
-	// users['user'+count]=socket.id;
-	
+	//
+	socket.on('myconnect', function(username){
+		users.addUser(username,socket.id);
+		console.log(username+' 登入,'+'在线人数'+users.userCount);
+		io.emit('notice_login',username);
+	});
 
+	//用户关闭连接事件
+	socket.on('disconnect', function(username){
+		users.deleteUser(username);
+		console.log('user disconnected');
+		io.emit('notice_logout',username);
+	});
+
+	//系统消息事件
+	// io.emit('notice_login',socket.id+"  加入会话");
+
+
+
+	//聊天室，聊天室所有用户看到
 	socket.on('chat', function(msg){
 		console.log('chat: ' + msg);
-		// 
+		//
 		io.emit('send', msg);
 	});
-	socket.on('send', function(msg){
+
+	//用户一对一发送事件(server发往client)
+	socket.on('send', function(jsonString){
+		var jsonObject=JSON.parse(jsonString);
+		var userName=jsonObject.username;
+
 		console.log('new message ############: ' + msg);
-		socket.emit('send','the message is'+msg);
+		if(users.hasUser(userName)){
+			var socketId=users.getUserId(userName);
+			socket(socketId).emit('send',jsonString);
+		}else{
+			socket.emit('notice_err','该用户已下线');
+		}
 	});
+	//server端获取(client发往server)
 	socket.on('get', function(msg){
 		console.log('new message ############: ' + msg);
 	});
 
-	socket.on('connect', function(username){
-		users[username]=socket.id;
-		console.log(username+' connected');
-	});
-	socket.on('disconnect', function(username){
-		console.log(username+' disconnected1');
-		delete users[username];
-		console.log("+++++++++++++++");
-		for(i in users){
-			console.log(i +": "+ users[i]);
-		}
-		console.log("--------------");
-	});
+
 });
+
+
 
 
 
